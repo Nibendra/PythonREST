f435e824ebb0d85858ad099a65ddad199e3e5a10
Nibendra <nibendrabajracharya@gmail.com>
2022-07-13 15:05:46 +0545

    Generics

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,3 @@
-import json
-from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.models import Product
@@ -9,5 +7,8 @@ from products.serializers import ProductSerializer
 
 @api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    data = request.data
-    return Response(data)
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "Not good data"}, status = 400)
